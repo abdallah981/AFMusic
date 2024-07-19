883355e7945b7956c586ecff9f396321598e82c9
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-07-19 20:04:57 +0300

    Update play.py

--- ZeMusic/utils/inline/play.py
+++ ZeMusic/utils/inline/play.py
@@ -24,36 +24,9 @@ selections = [
     "▃▅▂▅▃▇▄▅▃",
 ]
 
-def time_to_sec(time: str):
-    x = time.split(":")
-
-    if len(x) == 2:
-        min = int(x[0])
-        sec = int(x[1])
-
-        total_sec = (min*60) + sec
-    elif len(x) == 3:
-        hour = int(x[0])
-        min = int(x[1])
-        sec = int(x[2])
-
-        total_sec = (hour*60*60) + (min*60) + sec
-
-    return total_sec
 
 def stream_markup_timer(_, chat_id, played, dur):
-    played_sec = time_to_sec(played)
-    total_sec = time_to_sec(dur)
-
-    x, y = str(round(played_sec/total_sec,1)).split(".")
-    pos = int(y)
-
-    line = "▱"
-    circle = "▰"
-
-    bar = line*(pos-1)
-    bar += circle
-    bar += line*(8-len(bar))
+    bar = random.choice(selections)
     buttons = [
         [
             InlineKeyboardButton(
@@ -82,7 +55,7 @@ def stream_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text=config.CHANNEL_NAME, url=lnk
+                text="اقتباسات", url=lnk
             ),
         ],
         [
