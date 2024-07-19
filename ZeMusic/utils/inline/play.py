5ee44532bd82c30994b8eff7ac7bf9ed91769a9e
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-07-19 17:35:48 +0300

    Update play.py

--- ZeMusic/utils/inline/play.py
+++ ZeMusic/utils/inline/play.py
@@ -42,17 +42,13 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text=_["PL_B_2"],
-                callback_data=f"add_playlist {videoid}",
-            ),
-            InlineKeyboardButton(
-                text=_["PL_B_3"],
+                text= "‹ قائمة التحڪم ›",
                 callback_data=f"PanelMarkup {videoid}|{chat_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text=_["CLOSEMENU_BUTTON"], callback_data="close"
+                text= "اغلاق", callback_data="close"
             )
         ],
     ]
@@ -70,11 +66,11 @@ def telegram_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text=_["PL_B_3"],
+                text= "‹ قائمة التحڪم ›",
                 callback_data=f"PanelMarkup None|{chat_id}",
             ),
             InlineKeyboardButton(
-                text=_["CLOSEMENU_BUTTON"], callback_data="close"
+                text= "اغلاق", callback_data="close"
             ),
         ],
     ]
@@ -88,17 +84,13 @@ def stream_markup(_, videoid, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["PL_B_2"],
-                callback_data=f"add_playlist {videoid}",
-            ),
-            InlineKeyboardButton(
-                text=_["PL_B_3"],
+                text= "‹ قائمة التحڪم ›",
                 callback_data=f"PanelMarkup None|{chat_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text=_["CLOSEMENU_BUTTON"], callback_data="close"
+                text= "اغلاق", callback_data="close"
             )
         ],
     ]
@@ -109,11 +101,11 @@ def telegram_markup(_, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text=_["PL_B_3"],
+                text= "‹ قائمة التحڪم ›",
                 callback_data=f"PanelMarkup None|{chat_id}",
             ),
             InlineKeyboardButton(
-                text=_["CLOSEMENU_BUTTON"], callback_data="close"
+                text= "اغلاق", callback_data="close"
             ),
         ],
     ]
