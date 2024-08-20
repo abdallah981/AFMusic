a6c3163d0d0f16091446f31348e7e997ea49d0f7
Abdullah <62890648+king-1-9@users.noreply.github.com>
2024-08-20 18:16:42 +0300

    Update Gpt.py

--- ZeMusic/plugins/models/Gpt.py
+++ ZeMusic/plugins/models/Gpt.py
@@ -2,11 +2,10 @@ import random
 import time
 import requests
 from ZeMusic import app
-
 from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
-@app.on_message(filters.command(["سوال","ai","ask","gpt","solve"],  prefixes=["+", ".", "/", "-", "", "$","#","&"]))
+@app.on_message(filters.command(["لوي"], ""))
 async def chat_gpt(bot, message):
     try:
         start_time = time.time()
@@ -14,26 +13,24 @@ async def chat_gpt(bot, message):
 
         if len(message.command) < 2:
             await message.reply_text(
-                "• استخدم الأمر هكذا :\n\n • سوال + سؤالك"
+                "⟡ استخدم الأمر هكذا :\n\n ⟡ لوي + سؤالك"
             )
         else:
-            a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://chatgpt.apinepdev.workers.dev/?question={a}')
+            a = message.text.split(" ", 1)[1]  # هنا نضع محدد للمساحة
+            response = requests.get(f"https://chatgpt.apinepdev.workers.dev/?question={a}")
 
             try:
-                # Check if "results" key is present in the JSON response
                 if "answer" in response.json():
                     x = response.json()["answer"]
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                        f" {x} ",
+                        f"{x}",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
                     await message.reply_text("لم يتم العثور على النتائج في الاستجابة.")
             except KeyError:
-                # Handle any other KeyError that might occur
                 await message.reply_text("حدث خطأ أثناء الوصول إلى الاستجابة.")
     except Exception as e:
-        await message.reply_text(f"**á´‡Ê€Ê€á´Ê€: {e} ")
+        await message.reply_text(f"**Error: {e}**")
